a7f02f09d0110aec7f853044658f72256223001e
luonghuuthanhnam <luonghuuthanhnam5@gmail.com>
2022-10-31 09:52:02 +0700

    update post method,migrate params from url to body

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+from PaddleOCR.ppocr import data
 import e2e_process
 from kie_gcn import InvoiceGCN
 from PIL import Image
@@ -9,12 +11,17 @@ from IPython.display import display
 import time
 import torch.cuda
 import gc
+from pydantic import BaseModel
 
 from fastapi import FastAPI
 
+class ImgPath(BaseModel):
+    img_path: str
+
+
 app = FastAPI()
 
-print("***** Loading models...")
+print("*****Loading models...*****")
 load_model_time = time.time()
 orientationChecker = OrientationChecker( model_path= "./weights/orientation/invoice_rotation_220920.pth")
 e2e_OCR_Engine = e2e_process.E2E_OCR_Engine(
@@ -22,7 +29,7 @@ e2e_OCR_Engine = e2e_process.E2E_OCR_Engine(
     text_recognition_model_path="./weights/ocr/ocr_221026.pth",
     gcn_model_path="./weights/gcn/GCN_221027_best_state_dict.pth"
 )
-print(f"***** Models has been uploaded successfully in {time.time() - load_model_time} s")
+print(f"*****Models has been uploaded successfully in {time.time() - load_model_time} s*****")
 
 def extract_discharge_paper(img_path):
     image = cv2.imread(img_path)
@@ -37,8 +44,9 @@ def extract_discharge_paper(img_path):
 def read_root():
     return {"Papaya": "Success"}
 
-@app.post("/ocr_discharge_paper/")
-async def ocr_handeler(img_path):
-    print(img_path)
-    result = extract_discharge_paper(img_path)
+
+@app.post("/OCR/DischargePaper/")
+async def ocr_handeler(data: ImgPath):
+    print(data.img_path)
+    result = extract_discharge_paper(data.img_path)
     return result
